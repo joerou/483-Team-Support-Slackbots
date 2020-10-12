f979fdeb831b98fb2d6d3c0f8f58eb97983974b1
brendanhemstreet <brendanhemstreet@gmail.com>
2020-10-12 15:38:38 -0400

    test

--- app.py
+++ app.py
@@ -188,65 +188,61 @@ def sampleServey():
         api_response = slack_client.views_open(
             trigger_id=request.form["trigger_id"],
             view={
-            {
-                "label": "Choose a number 1-5",
-                "name": "survey",
-                "type": "select",
-                "option_groups":[
-                {
-                    "label": "I am the life of the party",
-                    "options": [
-                    {
-                        "label": "1",
-                        "value": "1"
+                    "type": "modal",
+                    "title": {
+                        "type": "plain_text",
+                        "text": "Sample Servey",
+                        "emoji": True
                     },
-                    {
-                        "label": "2",
-                        "value": "2"
+                    "submit": {
+                        "type": "plain_text",
+                        "text": "Submit",
+                        "emoji": True
                     },
-                    {
-                    "label": "3",
-                    "value": "3"
+                    "close": {
+                        "type": "plain_text",
+                        "text": "Cancel",
+                        "emoji": True
                     },
-                    {
-                    "label": "4",
-                    "value": "4"
-                    },
-                    {
-                    "label": "5",
-                    "value": "5"
-                    }
+                    "blocks": [
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": "Please select *True* _or_ *False*."
+                            }
+                        },
+                        {
+                            "type": "actions",
+                            "elements": [
+                                {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "True",
+                                        "emoji": True
+                                    },
+                                    "value": "True"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "actions",
+                            "elements": [
+                                {
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "False",
+                                        "emoji": True
+                                    },
+                                    "value": "False"
+                                }
+                            ]
+                        }
                     ]
-                },
-                {
-                    "label": "Feel little concerns for others",
-                    "options": [
-                    {
-                        "label": "1",
-                        "value": "1"
-                    },
-                    {
-                        "label": "2",
-                        "value": "2"
-                    },
-                    {
-                        "label": "3",
-                        "value": "3"
-                    },
-                    {
-                        "label": "4",
-                        "value": "4"
-                    },
-                    {
-                        "label": "5",
-                        "value": "5"
-                    }
-                    
-                    ]
-                },
-            ]
-         }
-        )
+                }
+            )
         return make_response("", 200)
     except SlackApiError as e:
         code = e.response["error"]
