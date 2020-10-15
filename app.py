174deac90324dbc7781612cf1f311f74d52d7fd1
Yilu Dong <yiludong0603@gmail.com>
2020-10-14 22:07:33 -0400

    Add code to store message in CosmosDB

--- app.py
+++ app.py
@@ -1,27 +1,66 @@
 import logging
 import os
 from slack_bolt import App
-from flask import Flask, request
 from slack_bolt.adapter.flask import SlackRequestHandler
+from flask import Flask, request
+from azure.cosmos import exceptions, CosmosClient, PartitionKey
 
-# logging
+###############################################################################
+# Initializing
+###############################################################################
+
+# enable logging
 logging.basicConfig(level=logging.DEBUG)
+
+# Initialize bolt
 bolt_app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-# Initialize a Flask app to host the events adapter
+# Initialize Flask app and bolt handler
 app = Flask(__name__)
 handler = SlackRequestHandler(bolt_app)
 
+# redirect request to bolt
 @app.route("/slack/events", methods=["POST"])
 def slack_events():
     return handler.handle(request)
 
+# Initialize the Cosmos client
+cosmos = CosmosClient(
+    endpoint=os.environ.get("AZURE_COSMOS_ENDPOINT"),
+    key=os.environ.get("AZURE_COSMOS_MASTER_KEY")
+)
+
+# Create a database
+database_name = 'bot-storage'
+database = cosmos.create_database_if_not_exists(id=database_name)
+
+# Create a container
+# Using a good partition key improves the performance of database operations.
+msgDB_name = 'message-storage'
+msgDB = database.create_container_if_not_exists(
+    id=msgDB_name, 
+    partition_key=PartitionKey(path="/user"),
+    offer_throughput=400
+)
+
 ###############################################################################
-# message handler
+# Message Handler
 ###############################################################################
+
+# Listing on all messages
+@bolt_app.message()
+def log_message(message):
+    msg = {
+        'timestamp' : message["ts"],
+        'channel': message["channel"],
+        'user': message["user"],
+        'message': message["text"],
+        'mention': None,
+    }
+    msgDB.create_item(msg)
 
 # Listens to incoming messages that contain "hello"
 @bolt_app.message("hello")
@@ -42,22 +81,19 @@ def message_hello(message, say):
         text=f"Hey there <@{message['user']}>!"
     )
 
+###############################################################################
+# Action Handler
+###############################################################################
+
 @bolt_app.action("button_click")
 def action_button_click(body, ack, say):
     # Acknowledge the action
     ack()
     say(f"<@{body['user']['id']}> clicked the button")
 
-# # Example responder to greetings
-# @slack_events_adapter.on("message")
-# def handle_message(event_data):
-#     message = event_data["event"]
-#     # If the incoming message contains "hi", then respond with a "Hello" message
-#     if message.get("subtype") is None and "hi" in message.get('text'):
-#         channel = message["channel"]
-#         message = "Hello <@%s>! :tada:" % message["user"]
-#         slack_client.chat_postMessage(channel=channel, text=message)
-
+###############################################################################
+# Event Handler
+###############################################################################
 
 # Example reaction emoji echo
 @bolt_app.event("reaction_added")
@@ -67,17 +103,13 @@ def reaction_added(event, say):
     text = ":%s:" % emoji
     say(channel=channel, text=text)
 
-
-
 #Triggering event upon new member joining
 @bolt_app.event("member_joined_channel")
-def new_member_survey(event):
+def new_member_survey(event, say):
     channel = event["channel"]
     user = event["user"]
     message = "Hello <@%s> Thanks for joining the chat!, Please take a personality survey with /survey :tada:" % user
     say(channel=channel, text=message)
-
-
 
 # Error events
 @bolt_app.event("error")
@@ -85,7 +117,7 @@ def error_handler(err):
     print("ERROR: " + str(err))
 
 ###############################################################################
-# slash command handler
+# Slash Command Handler
 ###############################################################################
 
 # Sample slash command "/hello"
@@ -106,7 +138,7 @@ def repeat_text(ack, say, command):
 
 # Sample slash command "/sampleservey"
 @bolt_app.command('/sampleServey')
-def sampleServey(ack, body):
+def sampleServey(ack, body, client, logger):
     ack()
     try:
         client.views_open(
@@ -173,7 +205,7 @@ def sampleServey(ack, body):
         
 #slash command for survey
 @bolt_app.command('/servey')
-def survey():
+def survey(ack, body, client, logger):
     ack()
     try:
         client.views_open(
@@ -229,5 +261,4 @@ def survey():
 # Once we have our event listeners configured, we can start the
 # Flask server with the default `/events` endpoint on port 3000
 if __name__ == "__main__":
-# slack_events_adapter.start(port=3000)
     app.run(debug=True, port=int(os.environ.get("PORT", 3000)))
