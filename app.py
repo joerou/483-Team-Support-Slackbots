d1bb1e719868b1203db39040a0978dd9d4f62742
brendanhemstreet <brendanhemstreet@gmail.com>
2020-10-17 18:33:24 -0400

    testing survey

--- app.py
+++ app.py
@@ -28,6 +28,75 @@ handler = SlackRequestHandler(bolt_app)
 def slack_events():
     return handler.handle(request)
 
+question1_payload = {
+    "type": "modal",
+# View identifier
+    "callback_id": "view_1",
+    "title": {"type": "plain_text", "text": "Question 1"},
+    
+    "blocks": [
+        {
+            "type": "section",
+            "text": {"type": "mrkdwn", "text": "I am the life of the party"},
+            "accessory": {
+                "type": "button",
+                "text": {"type": "plain_text", "text": "Next"},
+                "action_id": "button_abc"
+            }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "plain_text",
+            "text": "Choose a number 1-5 based on how this represents you"
+          },
+          "accessory": {
+            "type": "radio_buttons",
+            "action_id": "this_is_an_action_id",
+            
+            "options": [
+              {
+                "value": "Q1_1",
+                "text": {
+                  "type": "plain_text",
+                  "text": "1 Strongly Disagree"
+                }
+              },
+              {
+                "value": "Q1_2",
+                "text": {
+                  "type": "plain_text",
+                  "text": "2"
+                }
+              },
+              {
+                "value": "Q1_3",
+                "text": {
+                  "type": "plain_text",
+                  "text": "3"
+                }
+              },
+              {
+                "value": "Q1_4",
+                "text": {
+                  "type": "plain_text",
+                  "text": "4"
+                }
+              },
+              {
+                "value": "Q1_5",
+                "text": {
+                  "type": "plain_text",
+                  "text": "5 Strongly Agree"
+                }
+              }
+            ]
+          }
+        }
+        
+        
+    ]
+}
 # Create a logger for the 'azure' SDK and configure a console output
 azureLogger = logging.getLogger('azure')
 azureLogger.setLevel(logging.DEBUG)
@@ -127,75 +196,7 @@ def action_button_click(ack, body, client):
         # Pass a valid trigger_id within 3 seconds of receiving it
             trigger_id=body["trigger_id"],
         # View payload
-            view={
-                "type": "modal",
-            # View identifier
-                "callback_id": "view_1",
-                "title": {"type": "plain_text", "text": "Question 1"},
-                
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {"type": "mrkdwn", "text": "I am the life of the party"},
-                        "accessory": {
-                            "type": "button",
-                            "text": {"type": "plain_text", "text": "Next"},
-                            "action_id": "button_abc"
-                        }
-                    },
-                    {
-                      "type": "section",
-                      "text": {
-                        "type": "plain_text",
-                        "text": "Choose a number 1-5 based on how this represents you"
-                      },
-                      "accessory": {
-                        "type": "radio_buttons",
-                        "action_id": "this_is_an_action_id",
-                        
-                        "options": [
-                          {
-                            "value": "Q1_1",
-                            "text": {
-                              "type": "plain_text",
-                              "text": "1 Strongly Disagree"
-                            }
-                          },
-                          {
-                            "value": "Q1_2",
-                            "text": {
-                              "type": "plain_text",
-                              "text": "2"
-                            }
-                          },
-                          {
-                            "value": "Q1_3",
-                            "text": {
-                              "type": "plain_text",
-                              "text": "3"
-                            }
-                          },
-                          {
-                            "value": "Q1_4",
-                            "text": {
-                              "type": "plain_text",
-                              "text": "4"
-                            }
-                          },
-                          {
-                            "value": "Q1_5",
-                            "text": {
-                              "type": "plain_text",
-                              "text": "5 Strongly Agree"
-                            }
-                          }
-                        ]
-                      }
-                    }
-                    
-                    
-                ]
-            }
+            view=question1_payload
     )
 
 ###############################################################################
