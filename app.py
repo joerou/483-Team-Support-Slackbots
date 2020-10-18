d7a0318ebe49b754670f907d7a207abecd220122
DeathLyte <60337810+DeathLyte@users.noreply.github.com>
2020-10-18 16:49:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -969,12 +969,40 @@ def survey(ack, body, client):
 def psych_survey(ack, body, client):
 # Acknowledge the command request
     ack()
+# Call views_open with the built-in client
     client.views_open(
-        # Pass a valid trigger_id within 3 seconds of receiving it
+    # Pass a valid trigger_id within 3 seconds of receiving it
         trigger_id=body["trigger_id"],
-        # View payload
-        view=psych_q1_payload
-    )
+    # View payload
+        view={
+            "type": "modal",
+        # View identifier
+            "callback_id": "view_1",
+            "title": {"type": "plain_text", "text": "My App"},
+            "submit": {"type": "plain_text", "text": "Submit"},
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {"type": "mrkdwn", "text": "Welcome to a modal with _blocks_"},
+                    "accessory": {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "Click me!"},
+                        "action_id": "button_abc"
+                    }
+                },
+                {
+                    "type": "input",
+                    "block_id": "input_c",
+                    "label": {"type": "plain_text", "text": "What are your hopes and dreams?"},
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "dreamy_input",
+                        "multiline": True
+                    }
+                }
+            ]
+        }
+)
 
 ###############################################################################
 
