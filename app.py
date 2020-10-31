fc0c277660a33cd5abf719e83ff5bdb85e58c40b
Rjs96 <60358080+Rjs96@users.noreply.github.com>
2020-10-31 15:19:32 -0400

    Message Button

--- app.py
+++ app.py
@@ -925,15 +925,6 @@ def reaction_added(ack, event, say, client):
             ]     
     )
 
-    @bolt_app.action("feedback_button")
-    def action_button_click(ack, body, client):
-        # Acknowledge the action
-        user = body['user']['id']
-        client.chat_delete(channel = channel, timestamp = ts)
-        client.chat_postEphemeral(
-            channel = channel, 
-            user = user,
-            text = "Good Feedback")
 
 
 # Triggering event upon new member joining
