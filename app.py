e7bf0beb2efad65a0226e06c5563bbb71903c65b
brendanhemstreet <brendanhemstreet@gmail.com>
2020-11-22 14:48:49 -0500

    testing survey

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def message_rest(ack):
 def action_button_click(ack, body, client):
     # Acknowledge the action
     ack();
-    form_json = json.loads(request.form["payload"])
+    #form_json = json.loads(request.form["payload"])
     client.views_update(
             view_id=body["view"]["id"],
         # Pass a valid trigger_id within 3 seconds of receiving it
@@ -168,7 +168,7 @@ def action_button_click(ack, body, client):
                 "blocks": [
                     {
                         "type": "section",
-                        "text": {"type": "mrkdwn", "text": "hello %s" % (form_json["actions"][0]["selected_options"][0]["value"])},
+                        "text": {"type": "mrkdwn", "text": "hello %s" % (body['user']['id'])},
                         "accessory": {
                             "type": "button",
                             "text": {"type": "plain_text", "text": "Next"},
