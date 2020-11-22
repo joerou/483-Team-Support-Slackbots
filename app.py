4e29982f30256be28622dc3c3c1737597c808d96
brendanhemstreet <brendanhemstreet@gmail.com>
2020-11-22 17:28:58 -0500

    testing survey

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ def action_button_click(ack, body, client, say):
                 "blocks": [
                     {
                         "type": "section",
-                        "text": {"type": "mrkdwn", "text": "hello %s" % (form_json)},
+                        "text": {"type": "mrkdwn", "text": "hello %s" % (form_json['actions'][0]['selected_option']['value'])},
                         "accessory": {
                             "type": "button",
                             "text": {"type": "plain_text", "text": "Next"},
