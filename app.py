75813594d8079654d08a54c6830140b66a1e5ddd
Andrew Whitaker <acw123103478@gmail.com>
2020-11-23 12:58:39 -0500

    troubleshooting

--- app.py
+++ app.py
@@ -124,7 +124,17 @@ def log_message(payload, next):
             'mention': None
         }
         msgDB.create_item(msg)
-        # update_statistics(msg, statDB)
+        # update_statistics(msg, statDB)    # this line causes a ModuleNotFoundError with slack_bolt for unknown reasons.
+        # due to the above error, im trying just having the code here for now.
+
+        # Update workspace-wide statistics
+        prev_stats = statDB.read_item(item="1", partition_key="Workspace-wide stats")
+        prev_stats['total_workspace_messages'] += 1
+        statDB.replace_item("1", prev_stats)
+
+        # Update individual user statistics
+
+        # Update individual channel statistics
 
         if (brainstormOn == 1):
             msgBrain = {
