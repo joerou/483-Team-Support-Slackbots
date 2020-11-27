336fcb172d9661120d91dfc55fbd5ae9a5d500bb
Rjs96 <60358080+Rjs96@users.noreply.github.com>
2020-11-27 17:49:42 -0500

    Home w/ Stats

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from flask import Flask, request
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 from questions_payloads import *
 from statistics import update_statistics
-from app_home import *
 
 ###############################################################################
 # Initializing
@@ -1401,7 +1400,110 @@ def amy_home(ack, event, client, say):
     stats = list(statDB.read_all_items())
     totalMessages = stats.get("total_workspace_messages")
     StatsText = "*Statistics* \nBelow are some statistics from your group channel that you may be interested in!\n Total Messages Sent: %d" %(totalMessages)
-    
+
+    opening = """Welcome to the Amy Bot! I am here to help your team development and psychological saftey.
+            On this page you can customize certain funcitonalities to best suit your teams needs as well as
+            check out some interesting statistics from your channel that could help you identify certain things
+            and allow your team to be more efficient in their work. Also, check out the about tab to see what 
+            slash commands are available to you!\n\n\n"""
+            
+    app_home = {
+           "type":"home",
+           "blocks":[
+              {
+                 "type":"section",
+                 "text":{
+                    "type":"mrkdwn",
+                    "text": opening
+                 }
+              },
+              {
+                #Horizontal divider line 
+                "type": "divider"
+              },
+              {
+                  #Section with text and a button
+                  "type": "section",
+                  "text": {
+                    "type": "mrkdwn",
+                    "text": "*Brainstorming* \nWould you like to be reminded to revisit your brainstorming session a week after?"
+                  },
+                  "accessory": {
+                        "type": "radio_buttons",
+                        "action_id": "Brainstorm_Options",
+                
+                        "options": [
+                        {
+                            "value": "1",
+                            "text": {
+                            "type": "plain_text",
+                            "text": "Yes"
+                            }   
+                        },
+                        {
+                            "value": "0",
+                            "text": {
+                            "type": "plain_text",
+                            "text": "No"
+                            }
+                        }]
+                    }
+                },
+                #Horizontal divider line 
+                {
+                  "type": "divider"
+                },
+              {
+                  #Section with text and a button
+                  "type": "section",
+                  "text": {
+                    "type": "mrkdwn",
+                    "text": "*Weekly Survey* \nHow often would you like a psychological saftey check in?"
+                  },
+                  "accessory": {
+                        "type": "radio_buttons",
+                        "action_id": "Weekly_Survey",
+                
+                        "options": [
+                        {
+                            "value": "1",
+                            "text": {
+                            "type": "plain_text",
+                            "text": "Once a week"
+                            }   
+                        },
+                        {
+                            "value": "2",
+                            "text": {
+                            "type": "plain_text",
+                            "text": "Once every 2 weeks"
+                            }
+                        },
+                        {
+                            "value": "3",
+                            "text": {
+                            "type": "plain_text",
+                            "text": "Once every 3 weeks"
+                            }
+                        }]
+                    }
+                },
+                #Horizontal divider line 
+                {
+                  "type": "divider"
+                },
+              {
+                  #Section with text and a button
+                  "type": "section",
+                  "text": {
+                    "type": "mrkdwn",
+                    "text": StatsText
+                  }
+                  
+                }
+           ]
+        }
+
     client.views_publish(
         user_id = event["user"], 
         view = app_home)
