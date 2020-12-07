8f91fd64db78f40a1c8e1458e5359a5dfcaac9fe
brendanhemstreet <brendanhemstreet@gmail.com>
2020-12-07 15:35:13 -0500

    group leader tracking

--- app.py
+++ app.py
@@ -371,7 +371,7 @@ def message_rest(ack, client, message):
     ack()
     global group_leader_name
     group_leader_name = 'Brendan Hemstreet2'
-    client.chat_postMessage(channel=user['id'], text=f"Hey there <@{user['id']}>, {group_leader_name}")
+    client.chat_postMessage(channel=message['channel'], text=f"Hey there <@{user['id']}>")
     workspace_stats = statDB.read_item(item = "1", partition_key = "Workspace-wide stats")
     total_messages = workplace_stats.get("total_workspace_messages")
     
