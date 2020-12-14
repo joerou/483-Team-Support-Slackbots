0be1a6db871acd32341a83e04189edd25009eff9
brendanhemstreet <brendanhemstreet@gmail.com>
2020-12-13 23:11:42 -0500

    testing messages

--- app.py
+++ app.py
@@ -416,11 +416,11 @@ def message_rest(ack, client, message):
     average = total_messages/user_count
     most_messages = 0
     user_with_most = message['user']
-    client.chat_postMessage(channel=message['channel'], text="userss %d" % (user_count))
+    client.chat_postMessage(channel=message['channel'], text="users %d" % (user_count))
     
     
     if total_messages % 5 == 0:
-        
+        group_leader_name = 'Brendan Hemstreet6'
         total = total/(workspace_stats['total_workspace_messages']/100)
         for user in user_results:
             group_leader_name = 'Brendan Hemstreet4'
